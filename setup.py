736c7516154853a1a0af68f4b227dbfc59101f0d
Indosaram <freedom_zero@naver.com>
2021-08-15 22:42:52 +0900

    add setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="selenium_helper",
+    version="0.0.1",
+    description="Simple Selenium Load Helper Package",
+    url="https://github.com/Indosaram/selenium_helper.git",
+    install_requires=["selenium=='*'"],
+)
--- test/test_selenium_loader.py
+++ test/test_selenium_loader.py
@@ -1,2 +1,2 @@
-import pytest
+# import pytest
 
