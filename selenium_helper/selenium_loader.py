7e1917993396dda465e1bfc007ee91c61b7de9e0
Indosaram <freedom_zero@naver.com>
2021-08-24 01:02:32 +0900

    update user option

--- selenium_helper/selenium_loader.py
+++ selenium_helper/selenium_loader.py
@@ -40,7 +40,9 @@ class SeleniumLoader:
         options = webdriver.ChromeOptions()
 
         if user_options is None:
-            options.add_argument("headless")
+            options.add_argument("--headless")
+            options.add_argument("--no-sandbox")
+            options.add_argument("window-size=1920,1280")
             options.add_argument(
                 "user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
                 "AppleWebKit/537.36 (KHTML, like Gecko)"
